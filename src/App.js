44cf8eced16fe32bc41096766e7b5f8d893ec857
erwanitwish <quillec.erwan@gmail.com>
2020-07-23 11:02:28 +0200

    back-office created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import Login from "./components/backOffice/Login";
+import Dashboard from "./components/backOffice/Dashboard";
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Switch>
-          <Route path='/'>
+          <Route exact path='/'>
             <Home />
           </Route>
-          <Route path='/back-office'></Route>
+          <Route path='/back-office'>
+            <Login />
+          </Route>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
         </Switch>
       </div>
     </Router>
