5f2b2dae2bc51d5178901c148535bde4d2a246f4
erwanitwish <quillec.erwan@gmail.com>
2020-07-23 10:57:02 +0200

    axios added for link with database and react router for backoffice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 
 function App() {
   return (
-    <div className='App'>
-      <Home />
-    </div>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+          <Route path='/back-office'></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Project from "./Project";
 import Datas from "../datas/Datas.json";
 
 export default function Portfolio() {
+  const [error, setError] = useState();
+  const [Projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        const projectsList = await axios.get(`http://localhost:8080/project`);
+        setProjects(projectsList.data);
+      } catch (err) {
+        setError(err);
+      }
+    };
+    getProjects();
+  }, []);
+
   return (
     <div className='portfolio'>
       <h2 className='part-title title-right'>PORTFOLIO</h2>
-      {Datas.map((project) => (
+      {Projects.map((project) => (
         <Project
           number={project.Number}
-          title={project.Title}
-          description={project.Description}
+          title={project.title}
+          description={project.description}
           image={project.image}
-          linkReal={project.LinkReal}
+          linkReal={project.link}
         />
       ))}
     </div>
